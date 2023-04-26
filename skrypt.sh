18310773e358ecf5a6faf5f956bb5e1a3e2e01ac
DSW51406 <dsw51406@student.dsw.edu.pl>
2023-04-26 21:01:49 +0200

    skrypt.sh --logs

--- skrypt.sh
+++ skrypt.sh
@@ -3,5 +3,13 @@ echo "Wybierz opcje: "
 read input
     case $input in
         --date) echo "$(date)";;
+        --logs) 
+		for i in {1..100}
+			do
+				echo "log$i.txt" > log$i.txt
+				me=`basename "$0"`
+				echo $me >> log$i.txt
+				echo $(date) >> log$i.txt
+			done;;
         *) echo "Nie ma takiej opcji"
     esac
