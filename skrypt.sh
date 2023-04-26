fa60e28e51c9648390fc32a54afa0cf3614d20c1
DSW51406 <dsw51406@student.dsw.edu.pl>
2023-04-26 21:33:08 +0200

    Projekt cz.2

--- skrypt.sh
+++ skrypt.sh
@@ -2,8 +2,8 @@
 echo "Wybierz opcje: "
 read input
     case $input in
-        --date) echo "$(date)";;
-        --logs) 
+        --date|-d) echo "$(date)";;
+        --logs|-l) 
 		echo "ile plików log, pozostaw puste i wciśnij enter, aby utworzyć domyślne 100"
 		read var1
 		if [ -z $var1 ] 
@@ -24,9 +24,32 @@ read input
 				echo $(date) >> log$i.txt
 			done
 			fi;;
-		--help)
-		echo "Wpisz --date, aby wyświetlić aktualną datę"
-		echo "Wpisz --logs, a następnie, gdy zostaniesz zapytany, wpisz liczbę plików log, która ma zostać utworzona (pozostaw puste, aby wygenerować 100)"
-		echo "Wpisz --help, aby wyświetlić pomoc"
+		--error|-e)
+		echo "ile plików error, pozostaw puste i wciśnij enter, aby utworzyć domyślne 100"
+		read var2
+		if [ -z $var2 ] 
+		then
+		for i in {1..100}
+			do
+				echo "error$i.txt" > error$i.txt
+				me=`basename "$0"`
+				echo $me >> error$i.txt
+				echo $(date) >> error$i.txt
+			done
+			else
+			for i in $( seq 0 $var2 )
+			do
+				echo "error$i.txt" > error$i.txt
+				me=`basename "$0"`
+				echo $me >> error$i.txt
+				echo $(date) >> error$i.txt
+			done
+			fi;;
+
+		--help|h)
+		echo "Wpisz --date lub -d, aby wyświetlić aktualną datę"
+		echo "Wpisz --logs lub -l, a następnie, gdy zostaniesz zapytany, wpisz liczbę plików log, która ma zostać utworzona (pozostaw puste, aby wygenerować 100)"
+		echo "Wpisz --error lub -e, a następnie, gdy zostaniesz zapytany, wpisz liczbę plików error, która ma zostać utworzona (pozostaw puste, aby wygenerować 100)"
+		echo "Wpisz --help lub -h, aby wyświetlić pomoc"
         *) echo "Nie ma takiej opcji"
     esac
