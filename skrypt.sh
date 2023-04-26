ba4d3a06f16df25624a4f0fcd66829854efafdf8
DSW51406 <dsw51406@student.dsw.edu.pl>
2023-04-26 21:19:22 +0200

    skrypt.sh --logs z argumetem

--- skrypt.sh
+++ skrypt.sh
@@ -4,12 +4,26 @@ read input
     case $input in
         --date) echo "$(date)";;
         --logs) 
+		echo "ile plików log, pozostaw puste i wciśnij enter, aby utworzyć domyślne 100"
+		read var1
+		if [ -z $var1 ] 
+		then
 		for i in {1..100}
 			do
 				echo "log$i.txt" > log$i.txt
 				me=`basename "$0"`
 				echo $me >> log$i.txt
 				echo $(date) >> log$i.txt
-			done;;
+			done
+			else
+			for i in $( seq 0 $var1 )
+			do
+				echo "log$i.txt" > log$i.txt
+				me=`basename "$0"`
+				echo $me >> log$i.txt
+				echo $(date) >> log$i.txt
+			done
+			fi;;
+
         *) echo "Nie ma takiej opcji"
     esac
